1dcf721537bb505bba7fe18173dfb97d3d3e2548
jourthoma <pronetoussama@hotmail.fr>
2019-07-01 15:07:22 +0100

    images

--- src/main/java/com/koulwakel/koul/Web/WebImage.java
+++ src/main/java/com/koulwakel/koul/Web/WebImage.java
@@ -1,5 +1,9 @@
 package com.koulwakel.koul.Web;
 
+import java.awt.*;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
@@ -9,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import com.koulwakel.koul.Entites.Image;
 import com.koulwakel.koul.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -38,10 +43,17 @@ public class WebImage {
         return imageRepository.findAll();
     }
 
-    @RequestMapping(value = "/find_id_image/{idimage}", method = RequestMethod.GET)
+
+    @GetMapping(path = "/find_id_image/{idimage}", produces = MediaType.IMAGE_PNG_VALUE)
     @CrossOrigin(origins = "http://localhost:4200")
-    public Optional<Image> findIdimage(@PathVariable(value = "idimage") Integer idimage) {
-        return imageRepository.findById(idimage);
+    public byte[] findIdimage(@PathVariable("idimage") Integer idimage)
+    throws Exception
+    {
+
+       Image i =  imageRepository.findById(idimage).get();
+      return   Files.readAllBytes(Paths.get(System.getProperty("user.home")+"/koul/images/"+i.getLabelle()));
+
+
     }
 
 
